b78badb9213cd3762ae601d16f1953b50f739be3
Vatsal Sanjay <vatsalsanjay@gmail.com>
2023-10-22 17:06:58 +0200

    update with new variables

--- TestCases/ViscousOilOnWaterWaves.c
+++ TestCases/ViscousOilOnWaterWaves.c
@@ -3,19 +3,27 @@
 #include "../src-IceHydro/two-phase-IceHydro.h"
 #include "../src-IceHydro/reduced-IceHydro.h"
 
-/**
-The density and viscosity of the upper fluid is so low that it does not
-effect the flow inside the first one, resulting in a surface wave.
-*/
-#define Rhor (1e-1)
-#define MUr (1e-2)
+// note that here, ice is just a proxy for the thin layer on top of the bath!!
 
-// Galileo number: gL^3/(\mu/\rho)^2
-#define Ga 1e4
-// initial amplitude of the ocean wave
-#define A0 0.1
+// densities
+#define Rhor_IceOcean (1e0) // this is the density ratio of thin layer on top of ocean and ocean...
+#define Rhor_AirOcean (1e-1) // this is the density ratio of air to that of the ocean...
+
+// viscosities
+#define Ga 1e4 // Galileo number: gL^3/(\mu/\rho)^2 -- this is based on the viscosity of ocean
+#define Mur_IceOcean 1e0 // this is the viscosity ratio of thin layer on top of ocean and ocean...
+#define Mur_AirOcean 1e-2  // this is the viscosity ratio of air to that of the ocean...
+
+// initial amplitudes
+#define A0_OceanIce 0.1 // initial amplitude of the ocean-ice interface
+#define A0_IceAir 0.1 // initial amplitude of the ice-air interface
+
 // thickness of the ice sheet
 #define hIce 0.1
+
+// wave length of the surface waves.. lambda_Ocean is always 1. change lambda_IceAir to control the asymmetry of the standing waves
+#define lambda_Ocean 1.0 // fix this to 1 always.. this is the length scale of the problem
+#define lambda_IceAir 1.0 // this is the ratio of the wave length of the ice-air interface to that of the ocean-ice interface
 
 #define tmax (4.)
 #define tsnap (tmax/100.)
@@ -27,8 +35,11 @@ effect the flow inside the first one, resulting in a surface wave.
 char nameOut[80], amplitudeFile[80], name[80];
 static FILE * fp1 = NULL;
 static FILE * fp2 = NULL;
-#define MAXlevel 7
-#define MINlevel 0
+
+// grid resolution 
+#define MAXlevel 7 // maximum level of refinement
+#define MINlevel 0 // minimum level of refinement
+
 int LEVEL;
 
 uf.n[left]   = 0.;
@@ -40,8 +51,8 @@ uf.n[bottom] = 0.;
 The initial condition is a small amplitude plane wave of wavelength ($\lambda$)
 unity. This wavelength is the relevant length scale for this problem. Note that $k = 2\pi$ */
 event init (t = 0) {
-  fraction (f, - (y-hIce) + A0*cos (2.*pi*x));
-  fraction (fOcean, - (y) + A0*cos (2.*pi*x));
+  fraction (f, - (y-hIce) + A0_IceAir*cos (2.*pi*x/lambda_IceAir));
+  fraction (fOcean, - (y) + A0_OceanIce*cos (2.*pi*x/lambda_Ocean));
 }
 
 int main() {
@@ -55,8 +66,8 @@ int main() {
   LEVEL = MAXlevel;
   init_grid(1 << LEVEL);
   
-  rhoOcean = 1.0; rhoIce = 1.0; rhoAir = Rhor;
-  muOcean = 1./sqrt(Ga); muIce = 1./sqrt(Ga); muAir = MUr/sqrt(Ga);
+  rhoOcean = 1.0; rhoIce = Rhor_IceOcean; rhoAir = Rhor_AirOcean;
+  muOcean = 1./sqrt(Ga); muIce = Mur_IceOcean/sqrt(Ga); muAir = Mur_AirOcean/sqrt(Ga);
 
   G.y = -1.; // acceleration due to gravity
 
