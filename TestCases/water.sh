9823d6cfecb4a92a656cfaf62212ec7c066c8b45
ValeRos9 <123566828+ValeRos9@users.noreply.github.com>
2024-02-28 16:53:31 +0100

    Update water.sh
    Included video lines

--- TestCases/water.sh
+++ TestCases/water.sh
@@ -7,7 +7,6 @@ A0="0.01"
 tmax="2.0"
 DT="1e-3" 
 level="7" 
-g="-1.0"
 
 qcc -fopenmp -Wall -O2 WavesWater.c -o WavesWater -lm
 
@@ -15,3 +14,5 @@ qcc -fopenmp -Wall -O2 WavesWater.c -o WavesWater -lm
 export OMP_NUM_THREADS=8
 ./WavesWater $Rhor_AirOcean $Mur_AirOcean $Ga $A0 $tmax $DT $level $g
 
+python3 VideoWaterWaves.py
+ffmpeg -framerate 30 -pattern_type glob -i 'Video/*.png' -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -c:v libx264 -pix_fmt yuv420p Movie.mp4
