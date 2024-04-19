8efeccd33aff417d320510f3f6e59faa2f5191a8
ValeRos9 <123566828+ValeRos9@users.noreply.github.com>
2024-04-19 11:43:48 +0200

    Add files via upload
    Modified the shell scripts, so that they are able to do multiple variable sweeps

--- TestCases/oil.sh
+++ TestCases/oil.sh
@@ -2,8 +2,8 @@
 
 Rhor_IceOcean="1e0"
 Rhor_AirOcean="1e-3" 
-Ga="1e8"
-Mur_IceOcean=("1e2" "1e3" "1e4" "1e5" "1e6" "1e7" "1e8" "1e9" "1e10" "1e11" "1e12")
+Ga="1e9"
+Mur_IceOcean="1e5"
 Mur_AirOcean="1e-2" 
 A0_OceanIce="0.01"
 A0_IceAir="0.01"
@@ -11,44 +11,46 @@ hIce="0.1"
 lambda_Ocean="1.0"
 lambda_IceAir="1.0"
 
-tmax="20.0" 
-DT="1e-3" 
-level="7"
+tmax="20.0"
+DT=("1e-4" "1e-5" "1e-6") 
+level=("8" "9" "10")
 
 #Repository cases 
-start="2036"
-end="2046"
+folder="2152"
 
 #compile the file
 qcc -fopenmp -Wall -O2 ViscousOilOnWaterWaves.c -o ViscousOilOnWaterWaves -lm
 
 #Loop over the cases
-for i in `seq $start $end`;
-do
+for ((i1=0; i1<${#DT[*]}; i1++)); do
+    for ((i2=0; i2<${#level[*]}; i2++)); do
 
-#make a repository
-mkdir -p $i
-scp -r ViscousOilOnWaterWaves oil.sh $i/
+    #make a repository
+    mkdir -p $folder 
+    touch parameters.txt 
+    scp -r ViscousOilOnWaterWaves $folder/
 
+    #print out of the variables 
+    echo Rhor_IceOcean $Rhor_IceOcean Rhor_AirOcean $Rhor_AirOcean Ga $Ga Mur_IceOcean $Mur_IceOcean \
+        Mur_AirOcean $Mur_AirOcean A0_OceanIce $A0_OceanIce A0_IceAir $A0_IceAir hIce $hIce lambda_Ocean $lambda_Ocean \
+        lambda_IceAir $lambda_IceAir tmax $tmax DT ${DT[$i1]} level ${level[$i2]} > parameters.txt
 
-#print out of the variables 
-echo Rhor_IceOcean $Rhor_IceOcean Rhor_AirOcean $Rhor_AirOcean Ga $Ga Mur_IceOcean ${Mur_IceOcean[$i-$start]} \
-Mur_AirOcean $Mur_AirOcean A0_OceanIce $A0_OceanIce A0_IceAir $A0_IceAir hIce $hIce lambda_Ocean $lambda_Ocean \
-lambda_IceAir $lambda_IceAir tmax $tmax DT $DT level $level
+    #run the file 
+    export OMP_NUM_THREADS=8
+    ./ViscousOilOnWaterWaves $Rhor_IceOcean $Rhor_AirOcean $Ga $Mur_IceOcean \
+    $Mur_AirOcean $A0_OceanIce $A0_IceAir $hIce $lambda_Ocean \
+    $lambda_IceAir $tmax ${DT[$i1]} ${level[$i2]}
 
+    #Make a video
+    python3 VideoGeneralCase.py
+    ffmpeg -framerate 30 -pattern_type glob -i 'Video/*.png' -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -c:v libx264 -pix_fmt yuv420p Movie.mp4
 
-#run the file 
-export OMP_NUM_THREADS=8
-./ViscousOilOnWaterWaves $Rhor_IceOcean $Rhor_AirOcean $Ga ${Mur_IceOcean[$i-$start]} \
-$Mur_AirOcean $A0_OceanIce $A0_IceAir $hIce $lambda_Ocean \
-$lambda_IceAir $tmax $DT $level
+    #Moving files instead of copying
+    mv intermediate Video amplitude.dat logfile.dat Movie.mp4 parameters.txt  $folder/
+    mv $folder ViscousOilOnWaterWaves_
 
-#Make a video
-python3 VideoGeneralCase.py
-ffmpeg -framerate 30 -pattern_type glob -i 'Video/*.png' -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -c:v libx264 -pix_fmt yuv420p Movie.mp4
+    #increment folder
+    ((folder++))
 
-# Moving files instead of copying
-mv intermediate Video amplitude.dat logfile.dat Movie.mp4 $i/
-mv $i ViscousOilOnWaterWaves_
-
+     done
 done
--- TestCases/water.sh
+++ TestCases/water.sh
@@ -2,44 +2,50 @@
 
 #Physical variables
 Rhor_AirOcean="1e-3" 
-Mur_AirOcean="1e-2" 
-Ga="1e4"
-A0="0.1"
+Mur_AirOcean=("1e2" "1e3" "1e4" "1e5" "1e6" "1e7" "1e8" "1e9" "1e10" "1e11" "1e12" "1e13" "1e14" "1e15")
+Ga="1e9"
+A0="0.01"
 
-tmax="10.0"
+tmax="20.0"
 DT="1e-3" 
 level="7" 
 
 #Repository cases 
-start="1005"
-end="1005"
+folder="1006"
 
 #compile the file
 qcc -fopenmp -Wall -O2 WavesWater.c -o WavesWater -lm
 
 #Loop over the cases
-for i in `seq $start $end`;
-do
+for ((i1=0; i1<${#Ga[*]}; i1++)); do
+    for ((i2=0; i2<${#Mur_AirOcean[*]}; i2++)); do
 
-#make a repository
-mkdir -p $i
-scp -r WavesWater water.sh $i/
+    #make a repository
+    mkdir -p $folder 
+    touch parameters.txt 
+    scp -r WavesWater $folder/
 
-#print out of the variables 
-echo Rhor_AirOcean $Rhor_AirOcean Mur_AirOcean $Mur_AirOcean Ga ${Ga[$i-$start]}\
-A0 $A0 tmax $tmax DT $DT level $level
+    #print out of the variables 
+    echo Rhor_AirOcean $Rhor_AirOcean Mur_AirOcean ${Mur_AirOcean[$i2]} \
+        Ga ${Ga[$i1]} A0 $A0 \
+        tmax $tmax DT $DT level $level > parameters.txt
 
-#run the file 
-export OMP_NUM_THREADS=8
-./WavesWater $Rhor_AirOcean $Mur_AirOcean ${Ga[$i-$start]} $A0 $tmax $DT $level
+    #run the file 
+    export OMP_NUM_THREADS=8
+    ./WavesWater $Rhor_AirOcean ${Mur_AirOcean[$i2]} ${Ga[$i1]} \
+    $A0 $tmax $DT $level \
+    
 
-#Make a video
-python3 VideoWaterWaves.py
-ffmpeg -framerate 30 -pattern_type glob -i 'Video/*.png' -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -c:v libx264 -pix_fmt yuv420p Movie.mp4
+    #Make a video
+    python3 VideoWaterWaves.py
+    ffmpeg -framerate 30 -pattern_type glob -i 'Video/*.png' -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -c:v libx264 -pix_fmt yuv420p Movie.mp4
 
-# Moving files instead of copying
-mv intermediate Video amplitude.dat logfile.dat Movie.mp4 $i/
-mv $i WavesWater_
+    #Moving files instead of copying
+    mv intermediate Video amplitude.dat logfile.dat Movie.mp4 parameters.txt  $folder/
+    mv $folder WavesWater_
 
+    #increment folder
+    ((folder++))
+
+     done
 done
-
